35c7934042a4e2b96d4bb22a3fbdf707a740c5b1
madokanomi <160171970+madokanomi@users.noreply.github.com>
2024-04-17 17:58:49 -0300

    Add files via upload

---
+++ At_if_else/script.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        <?php
+$nome = $_POST['nome'];
+$n1 = $_POST['n1'];
+$n2 = $_POST['n2'];
+
+$media = ($n1+$n2) / 2;
+
+if ($media > 7) {
+    echo "$nome Aprovado, essa é sua média: $media";
+} elseif($media > 5){
+echo "Recuperação, essa é sua $media";
+}
+else{
+    echo "$nome Reprovado, essa é a sua média: $media";
+}
+?>
+        </script>
+</head>
+<body>
+    
+</body>
+</html>
