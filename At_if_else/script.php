992c1a0e93eac270d05b34b7314d357099139604
madokanomi <160171970+madokanomi@users.noreply.github.com>
2024-04-19 15:06:34 -0300

    atualizacao GAMES

--- At_if_else/script.php
+++ At_if_else/script.php
@@ -1,29 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <script>
         <?php
-$nome = $_POST['nome'];
-$n1 = $_POST['n1'];
-$n2 = $_POST['n2'];
+$temp = $_POST['temp'];
 
-$media = ($n1+$n2) / 2;
-
-if ($media > 7) {
-    echo "$nome Aprovado, essa é sua média: $media";
-} elseif($media > 5){
-echo "Recuperação, essa é sua $media";
+if ($temp < 0) {
+    echo "Frio Congelante!! ";
+} elseif($temp >= 0 && $temp <=15){
+echo "Muito Frio !!";
+}
+elseif ($temp <= 15 && $temp < 25){
+    echo "Clima ameno";
 }
 else{
-    echo "$nome Reprovado, essa é a sua média: $media";
+echo "Calor";
 }
 ?>
         </script>
 </head>
 <body>
-    
+<header>
+        <nav>
+            <p class="logo">Medidor de Temperatura</p>
+            <ul class="nav-list">
+                </ul>
+            </nav>
+            </header>
+    <center>
+        <br>
+    <fieldset>
+    <form action="script.php" method="post">
+        <br>
+        <label for ="">Digite a Temperatura</label>      <br><br><br><br><br><br><br><br><br>
+<input type="number" name="temp" id="temp">      <br>      <br><br>
+
+        <button type="submit">Enviar</button><br><br><br><br>
+ </form></fieldset></center>
 </body>
 </html>
